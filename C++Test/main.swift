970dd7f4df347b77972881647d253b3b0a1f35d9
Stuart McIntosh <mcintst@yahoo.com>
2015-05-06 18:17:55 +0800

    Initial Commit

---
+++ C++Test/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  C++Test
+//
+//  Created by Stuart McIntosh on 6/5/15.
+//  Copyright (c) 2015 Stuart McIntosh. All rights reserved.
+//
+
+import Foundation
+
+println("Hello, World!")
+
